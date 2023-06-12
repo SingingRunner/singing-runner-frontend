570ae1163738a9e18c566e8d89b2d8927eeedb34
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-12 14:54:41 +0900

    Modify: [#9] 모든 곡 loading 완료 시 음원 재생

--- pages/testpage/index.tsx
+++ pages/testpage/index.tsx
@@ -1,50 +1,58 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-const PlaySongsSimultaneously: React.FC = () => {
-  const songFiles = [
-    // '/music/snowflower_origin.wav',
-    // '/music/snowflower_3keydown.wav',
-    // '/music/snowflower_3keyup.wav',
-    // '/music/snowflower_5keyup.wav',
-    // '/music/snowflower_2keyup.wav',
-    // '/music/snowflower_8keyup.wav',
-    '/music/snowflower_vocal_origin.wav',
-    '/music/snowflower_vocal_3keydown.wav',
-    '/music/snowflower_vocal_3keyup.wav',
-    // '/music/snowflower_origin.wav',
-    // '/music/snowflower_3keydown.wav',
-    // '/music/snowflower_3keyup.wav',
-  ];
+const songFiles = [
+  "/music/snowflower_origin.wav",
+  "/music/snowflower_3keydown.wav",
+  "/music/snowflower_3keyup.wav",
+  "/music/snowflower_5keyup.wav",
+  "/music/snowflower_2keyup.wav",
+  "/music/snowflower_8keyup.wav",
+  "/music/snowflower_vocal_origin.wav",
+  "/music/snowflower_vocal_3keydown.wav",
+  "/music/snowflower_vocal_3keyup.wav",
+  "/music/snowflower_origin.wav",
+  "/music/snowflower_3keydown.wav",
+  "/music/snowflower_3keyup.wav",
+];
 
+const PlaySongsSimultaneously = () => {
+  const [completeLoading, setCompleteLoading] = useState(false);
   const audioCtxRef = useRef<AudioContext | null>(null);
   const gainNodes = useRef<GainNode[]>([]);
   const sources = useRef<AudioBufferSourceNode[]>([]);
-  
-  useEffect(() => {
-    // If not running in the browser, don't execute the rest of the code
-    if (typeof window === 'undefined') return;
 
-    // Create AudioContext
-    audioCtxRef.current = new (window.AudioContext || window.webkitAudioContext)();
-
-    // Use the created AudioContext
-    const audioCtx = audioCtxRef.current;
-
-    Promise.all(songFiles.map((file) => fetch(file).then(res => res.arrayBuffer())))
-      .then(datas => Promise.all(datas.map(data => audioCtx.decodeAudioData(data))))
-      .then(buffers => {
+  const loadSong = (audioCtx: AudioContext) => {
+    Promise.all(
+      songFiles.map((file) => fetch(file).then((res) => res.arrayBuffer()))
+    )
+      .then((datas) =>
+        Promise.all(datas.map((data) => audioCtx.decodeAudioData(data)))
+      )
+      .then((buffers) => {
         gainNodes.current = songFiles.map((_, i) => {
           const gainNode = audioCtx.createGain();
           gainNode.gain.value = 0; // Start all audios muted
           const source = audioCtx.createBufferSource();
           source.buffer = buffers[i];
           source.connect(gainNode).connect(audioCtx.destination);
-          source.loop = true;
-          source.start();
           sources.current[i] = source;
           return gainNode;
         });
+        setCompleteLoading(true);
       });
+  };
+
+  useEffect(() => {
+    // If not running in the browser, don't execute the rest of the code
+    if (typeof window === "undefined") return;
+
+    // Create AudioContext
+    audioCtxRef.current = new (window.AudioContext ||
+      window.webkitAudioContext)();
+
+    // Use the created AudioContext
+    const audioCtx = audioCtxRef.current;
+    loadSong(audioCtx);
 
     // Cleanup function to stop all songs
     return () => {
@@ -54,7 +62,16 @@ const PlaySongsSimultaneously: React.FC = () => {
       gainNodes.current = [];
       sources.current = [];
     };
-  }, [songFiles]);
+  }, []);
+
+  useEffect(() => {
+    if (completeLoading) {
+      // If all songs are loaded, start playing them
+      sources.current.forEach((source) => {
+        source.start();
+      });
+    }
+  }, [completeLoading]);
 
   const handleSolo = (index: number) => {
     gainNodes.current.forEach((gainNode, i) => {
