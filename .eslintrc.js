2547cf41e2dac3bbe236486278e8f3fba088627b
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-13 21:30:43 +0900

    Setting: [#9] add eslint ignore rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
     "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/promise-function-async": "off",
+    "@typescript-eslint/no-floating-promises": "off",
   },
   settings: {
     react: {
