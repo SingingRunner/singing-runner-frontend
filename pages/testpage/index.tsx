8826085368f5eb786782c1d677c599ee129af3c8
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-13 14:41:56 +0900

    Feat: [#9] game component 상위 페이지 추가

--- pages/testpage/index.tsx
+++ pages/testpage/index.tsx
@@ -1,93 +1,62 @@
-import { useEffect, useRef, useState } from "react";
-
-const songFiles = [
-  "/music/snowflower_origin.wav",
-  "/music/snowflower_3keydown.wav",
-  "/music/snowflower_3keyup.wav",
-  "/music/snowflower_5keyup.wav",
-  "/music/snowflower_2keyup.wav",
-  "/music/snowflower_8keyup.wav",
-  "/music/snowflower_vocal_origin.wav",
-  "/music/snowflower_vocal_3keydown.wav",
-  "/music/snowflower_vocal_3keyup.wav",
-  "/music/snowflower_origin.wav",
-  "/music/snowflower_3keydown.wav",
-  "/music/snowflower_3keyup.wav",
-];
-
-const PlaySongsSimultaneously = () => {
-  const [completeLoading, setCompleteLoading] = useState(false);
-  const audioCtxRef = useRef<AudioContext | null>(null);
-  const gainNodes = useRef<GainNode[]>([]);
-  const sources = useRef<AudioBufferSourceNode[]>([]);
-
-  const loadSong = (audioCtx: AudioContext) => {
-    Promise.all(
-      songFiles.map((file) => fetch(file).then((res) => res.arrayBuffer()))
-    )
-      .then((datas) =>
-        Promise.all(datas.map((data) => audioCtx.decodeAudioData(data)))
-      )
-      .then((buffers) => {
-        gainNodes.current = songFiles.map((_, i) => {
-          const gainNode = audioCtx.createGain();
-          gainNode.gain.value = 0; // Start all audios muted
-          const source = audioCtx.createBufferSource();
-          source.buffer = buffers[i];
-          source.connect(gainNode).connect(audioCtx.destination);
-          sources.current[i] = source;
-          return gainNode;
-        });
-        setCompleteLoading(true);
-      });
-  };
+import { useEffect, useState } from "react";
+import PitchAnalyzer from "./temp_component/pitch";
+import PlaySongsSimultaneously from "./temp_component/PlaySongsSimultaneously";
+//testpage
+export default function TestPage() {
+  const [isLoadComplete, setLoadComplete] = useState(false);
+  const [isKeyUp, setKeyUp] = useState(false);
+  const [isKeyDown, setKeyDown] = useState(false);
+  const [isFrozen, setFrozen] = useState(false);
+  const [isMute, setMute] = useState(false);
+  let ans1Array: number[] | null = null;
+  let ans2Array: number[] | null = null;
+  let ans3Array: number[] | null = null;
 
   useEffect(() => {
-    // If not running in the browser, don't execute the rest of the code
-    if (typeof window === "undefined") return;
-
-    // Create AudioContext
-    audioCtxRef.current = new (window.AudioContext ||
-      window.webkitAudioContext)();
-
-    // Use the created AudioContext
-    const audioCtx = audioCtxRef.current;
-    loadSong(audioCtx);
-
-    // Cleanup function to stop all songs
-    return () => {
-      sources.current.forEach((source) => {
-        source.stop();
-      });
-      gainNodes.current = [];
-      sources.current = [];
+    const fetchFiles = async () => {
+      try {
+        const ans1 = await fetch("/origin.txt");
+        const ans2 = await fetch("/keyUp.txt");
+        const ans3 = await fetch("/keyDown.txt");
+        const ans1Text = await ans1.text();
+        const ans2Text = await ans2.text();
+        const ans3Text = await ans3.text();
+        ans1Array = ans1Text.split(",").map((value) => {
+          return Number(value);
+        });
+        ans2Array = ans2Text.split(",").map((value) => {
+          return Number(value);
+        });
+        ans3Array = ans3Text.split(",").map((value) => {
+          return Number(value);
+        });
+      } catch (err) {
+        console.log(err);
+      }
     };
+    fetchFiles();
   }, []);
 
-  useEffect(() => {
-    if (completeLoading) {
-      // If all songs are loaded, start playing them
-      sources.current.forEach((source) => {
-        source.start();
-      });
-    }
-  }, [completeLoading]);
-
-  const handleSolo = (index: number) => {
-    gainNodes.current.forEach((gainNode, i) => {
-      gainNode.gain.value = i === index ? 1 : 0;
-    });
-  };
-
   return (
-    <div>
-      {songFiles.map((_, index) => (
-        <button key={index} onClick={() => handleSolo(index)}>
-          Solo {index + 1}
-        </button>
-      ))}
-    </div>
+    <>
+      <PlaySongsSimultaneously
+        isLoadComplete={isLoadComplete}
+        setLoadComplete={setLoadComplete}
+      />
+      <PitchAnalyzer
+        isLoadComplete={isLoadComplete}
+        originAnswer={ans1Array}
+        keyUpAnswer={ans2Array}
+        keyDownAnswer={ans3Array}
+        isKeyUp={false}
+        isKeyDown={false}
+        isFrozen={false}
+        isMute={false}
+        setKeyUp={setKeyUp}
+        setKeyDown={setKeyDown}
+        setFrozen={setFrozen}
+        setMute={setMute}
+      />
+    </>
   );
-};
-
-export default PlaySongsSimultaneously;
+}
