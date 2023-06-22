bb7b1785f46fa204eead49501315bc675094352f
KoziKim <kory0711@gmail.com>
2023-06-23 05:12:03 +0900

    Feat: [#133] 로그아웃 기능 추가

--- src/components/units/initial/initial.container.tsx
+++ src/components/units/initial/initial.container.tsx
@@ -3,26 +3,16 @@ import SignupUI from "./initial.presenter";
 import { IInitialUIProps } from "./initial.types";
 
 export default function Initial() {
-  // const [isSignupClicked, setIsSignupClicked] = useState(false);
-
-  // const dummyClick = () => {
-  //   console.log("dummy");
-  // };
 
   const handleSignupClick = () => {
-    router.push("/signup")
+    router.push("/signup");
   };
 
   const handleLoginClick = () => {
-    router.push("/login")
+    router.push("/login");
   };
 
   const router = useRouter();
-  
-  // const handleChangeAddress = () => {
-  //   // 메인 화면으로 전환
-  //   router.push("/main");
-  // };
 
   const props: IInitialUIProps = {
     handleSignupClick,
--- src/components/units/myroom/myroomsetting/MyRoomSetting.types.ts
+++ src/components/units/myroom/myroomsetting/MyRoomSetting.types.ts
@@ -4,4 +4,5 @@ export interface IMyRoomSettingUIProps {
   onClickLogout: () => void;
   isLogoutClicked: boolean;
   onClickFinalLogout: () => void;
+  onClickCancelLogout: () => void;
 }
