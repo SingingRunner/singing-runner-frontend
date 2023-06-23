ae928f74ff4ea7e06e016ef646fc0817a652e17b
e-juhee <juhee971204@gmail.com>
2023-06-24 07:24:53 +0900

    Modify: [#74] 프로필 카드 컴포넌트에 친구 요청 API 연동

--- src/components/commons/profileCard/ProfileCard.tsx
+++ src/components/commons/profileCard/ProfileCard.tsx
@@ -1,33 +1,57 @@
+import { useMutation } from "@apollo/client";
 import * as S from "./ProfileCard.styles";
-
-interface IProfileCardProps {
-  character: string;
-  online?: boolean;
-  offline?: boolean;
-  add?: boolean;
-  nickname?: string;
-  tier?: string;
-  children?: React.ReactNode;
-  onClick?: () => void;
-  margin?: string;
-}
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { userIdState } from "../../../commons/store";
+import Modal from "../modal/Modal";
+import { IProfileCardProps } from "./ProfileCard.types";
+import { ADD_FRIEND } from "./ProfileCard.queries";
 
 export default function ProfileCard(props: IProfileCardProps) {
+  const [userId, setUserId] = useRecoilState(userIdState);
+  useEffect(() => {
+    setUserId(localStorage.getItem("userId") || "");
+  }, []);
+
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const [addFrinedModalIsOpen, setAddFriendModalIsOpen] = useState(false);
+
   const onClickAddFriend = () => {
-    // if (!props.add) return;
+    if (!props.add) return;
+    addFriend({
+      variables: {
+        addFriendDto: {
+          userId,
+          firendId: props.friendId,
+        },
+      },
+    });
+    setAddFriendModalIsOpen(true);
   };
 
   return (
-    <S.Wrapper style={{ margin: props.margin }}>
-      <S.ImgWrapper onClick={onClickAddFriend}>
-        <S.Img src={`/game/player/profile/${props.character}.png`} />
-        {props.add && <S.AddIcon src="/icon/add.png" />}
-        {props.online && <S.OnlineIcon />}
-        {props.offline && <S.OfflineIcon />}
-      </S.ImgWrapper>
-      {props.nickname && <S.Nickname>{props.nickname}</S.Nickname>}
-      {props.tier && <S.Tier src={`/tier/${props.tier}.png`} />}
-      {props.children && props.children}
-    </S.Wrapper>
+    <>
+      {addFrinedModalIsOpen && (
+        <Modal
+          buttonText="확인"
+          isCheck
+          hilightText={props.nickname}
+          firstText="님에게"
+          secondText="친구 요청을 보냈어요!"
+          onClickRight={() => setAddFriendModalIsOpen(false)}
+        />
+      )}
+      <S.Wrapper style={{ margin: props.margin }}>
+        <S.ImgWrapper onClick={onClickAddFriend}>
+          <S.Img src={`/game/player/profile/${props.character}.png`} />
+          {props.add && <S.AddIcon src="/icon/add.png" />}
+          {props.online && <S.OnlineIcon />}
+          {props.offline && <S.OfflineIcon />}
+        </S.ImgWrapper>
+        {props.nickname && <S.Nickname>{props.nickname}</S.Nickname>}
+        {props.tier && <S.Tier src={`/tier/${props.tier}.png`} />}
+        {props.children && props.children}
+      </S.Wrapper>
+    </>
   );
 }
