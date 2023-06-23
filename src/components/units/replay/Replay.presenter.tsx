b4fd124562e17480936626a10f7cf3770269850e
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-23 16:18:03 +0900

    Feat: [#136] 조회하는 id 따라서 세팅 버튼, 프로필 렌더링 유무 결정

--- src/components/units/replay/Replay.presenter.tsx
+++ src/components/units/replay/Replay.presenter.tsx
@@ -2,45 +2,57 @@
 import Button, { buttonType } from "../../commons/button/Button";
 import ButtonWrapper from "../../commons/buttons/wrapper";
 import InfiniteScroll from "react-infinite-scroller";
+import { useRecoilValue } from "recoil";
+import { userInfoState } from "../../../commons/store";
+import { useRouter } from "next/router";
+import ListItem from "../../commons/listItem/ListItem";
 import * as S from "./Replay.styles";
 
 export default function ReplayUI() {
+  const userInfo = useRecoilValue(userInfoState);
+  const currentId = userInfo.userId;
+  const router = useRouter();
+  const { userId } = router.query;
+  const isMyReplay = currentId === userId;
   const onLoadMore = () => {
     console.log("onLoadMore");
   };
 
   return (
     <>
-      <img
-        onClick={() => console.log("click!")}
-        style={{
-          position: "absolute",
-          height: "40px",
-          width: "auto",
-          top: "8px",
-          right: "8px",
-        }}
-        src="/icon/setting.png"
-      />
-      <div
-        style={{
-          display: "flex",
-          width: "100%",
-          height: "auto",
-          position: "relative",
-        }}
-      >
+      {isMyReplay ? (
         <img
+          onClick={() => console.log("click!")}
           style={{
             position: "absolute",
-            width: "46px",
-            left: "4px",
-            top: "-4px",
+            height: "40px",
+            width: "auto",
+            top: "8px",
+            right: "8px",
           }}
-          src="/images/yellowcat.png"
-          alt="logo"
+          src="/icon/setting.png"
         />
-      </div>
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "50px",
+            position: "relative",
+          }}
+        >
+          <img
+            style={{
+              position: "absolute",
+              width: "46px",
+              left: "4px",
+              top: "-4px",
+            }}
+            src="/images/yellowcat.png"
+            alt="logo"
+          />
+        </div>
+      )}
       <InfiniteScroll loadMore={onLoadMore}>
         <S.Container>
           <div
