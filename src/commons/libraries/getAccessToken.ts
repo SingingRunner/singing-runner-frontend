b4282d049de94e11ac82feb65307ca92ab552453
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-24 01:21:37 +0900

    Setting: tsconfig compile option 추가, request url 수정

--- src/commons/libraries/getAccessToken.ts
+++ src/commons/libraries/getAccessToken.ts
@@ -11,9 +11,9 @@ const REFRESH_ACCESS_TOKEN = gql`
 
 export const getAccessToken = async (): Promise<string | undefined> => {
   try {
-    const graphQLClient = new GraphQLClient( "http://localhost:3000/graphql", // 로컬 테스트용
-    // const graphQLClient = new GraphQLClient(
-      // "https://injungle.shop/api/graphql", // 서버 테스트용
+    // const graphQLClient = new GraphQLClient( "http://localhost:3000/graphql", // 로컬 테스트용
+    const graphQLClient = new GraphQLClient(
+      "https://injungle.shop/api/graphql", // 서버 테스트용
       { credentials: "include" }
     );
     const result = await graphQLClient.request<
