d1bd59e402910f7b17297c528396fc6f2392dec3
KoziKim <kory0711@gmail.com>
2023-06-25 06:03:50 +0900

    Modify: [#181] 모달 기존 방식에서 공통 컴포넌트로 수정

--- src/components/units/main/modals/MatchingModal.tsx
+++ src/components/units/main/modals/MatchingModal.tsx
@@ -1,46 +1,19 @@
-import * as S from "../Main.styles";
+import Modal from '../../../commons/modal/Modal';
 import { IMainUIProps } from "../Main.types";
 
 export default function MatchingModal(props: IMainUIProps) {
   return (
     <>
-      <S.ModalBackground>
-        <S.ModalWrapper>
-          <S.ImageWrapper>
-            <S.ImageEllipse src="/images/Ellipse.png" />
-            <S.ImageVector src="/images/Vector.png" />
-          </S.ImageWrapper>
-          <S.ModalMatchComplete>매칭이 완료되었습니다.</S.ModalMatchComplete>
-          <S.ModalMatchSongArtist>
-            {/* {props.singer ? props.singer : "강산에"} */}
-            Various Artists
-          </S.ModalMatchSongArtist>
-          <S.MarqueeContainer>
-            <S.MarqueeContent>
-              <S.ModalMatchSongTitle>
-                {/* {props.songTitle
-                      ? props.songTitle
-                      : "거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼"} */}
-                {"짱구는 못말려 오프닝(99년도 비디오판)"}
-              </S.ModalMatchSongTitle>
-              <S.ModalMatchSongTitle>
-                {/* {props.songTitle
-                      ? props.songTitle
-                      : "거꾸로 강을 거슬러 오르는 저 힘찬 연어들처럼"} */}
-                {"짱구는 못말려 오프닝(99년도 비디오판)"}
-              </S.ModalMatchSongTitle>
-            </S.MarqueeContent>
-          </S.MarqueeContainer>
-          <S.MatchButtonWrapper>
-            <S.MatchDenyButton onClick={props.handleMatchDecline}>
-              거절하기
-            </S.MatchDenyButton>
-            <S.MatchAcceptButton onClick={props.handleMatchAccept}>
-              참여하기
-            </S.MatchAcceptButton>
-          </S.MatchButtonWrapper>
-        </S.ModalWrapper>
-      </S.ModalBackground>
+      <Modal
+        isCheck={true}
+        firstText="매칭이 완료되었습니다."
+        singer={props.singer || "매칭 완료 가수"}
+        songTitle={props.songTitle || "이 메세지가 나온다는 것은 DB에 노래가 없다는 뜻이겠지요."}
+        buttonText="참여하기"
+        leftButtonText="거절하기"
+        onClickRight={props.handleMatchAccept}
+        onClickLeft={props.handleMatchDecline}
+      />
     </>
   );
 }
