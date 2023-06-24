3e5f83157d694099bd1988f14428c0689eb4d0ad
Park HyeongIn <hyeongin2024@gmail.com>
2023-06-24 21:07:30 +0900

    Feat: [#170] 리플레이 공개 유무 분기

--- src/components/units/replay/Replay.presenter.tsx
+++ src/components/units/replay/Replay.presenter.tsx
@@ -59,8 +59,12 @@ export default function ReplayUI(props: IReplayUIProps) {
               isSettingMode ? (
                 <ListItem
                   key={elem.replayId}
-                  buttonText="공개 유무 받아와서 보여주기 또 값으로도 저장해야함"
-                  buttonType={buttonType.SHORT_PINK}
+                  buttonText={elem.isPublic ? "공개" : "비공개"}
+                  buttonType={
+                    elem.isPublic
+                      ? buttonType.SHORT_SELECT
+                      : buttonType.SHORT_SELECT_EMPTY
+                  }
                   onClick={
                     props.setPublic
                       ? () => props.setPublic(elem.replayId, elem.isPublic)
