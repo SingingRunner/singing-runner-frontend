b6f754964d0488f8143735b2291b6496571fa965
KoziKim <kory0711@gmail.com>
2023-06-28 21:15:56 +0900

    Modify: [#350] 로그인 헤더, 친구추가, 친구관리 아이콘 효과음 추가

--- src/components/commons/layout/header/LoginHeader.tsx
+++ src/components/commons/layout/header/LoginHeader.tsx
@@ -1,12 +1,23 @@
 import { useRouter } from "next/router";
+import { useEffect, useRef } from 'react';
 import * as S from "./LoginHeader.styles";
 
 export default function LoginHeader() {
   const router = useRouter();
 
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    audioRef.current = new Audio("/sound/effect/pop.mp3");
+  }, []);
+
   const goBack = () => {
+    audioRef.current?.play();
     router.push("/");
   };
+
+
+
   return (
     <S.ImageWrapper>
       <S.BackButton
